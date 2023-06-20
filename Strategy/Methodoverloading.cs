722b96732beb952f9b77b3981a7a1c7393133195
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-20 18:05:01 +0530

    Add project files.

---
+++ Strategy/Methodoverloading.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    // METHOD OVERLOADING PROGRAM
+    public class Methodoverloading
+    {
+
+        public int Guide(int x,int y)
+        {
+            return x - y;
+        }
+        public double Guide(double x,double y)
+        {
+            return x * y;
+        }
+        
+        
+
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// OVERLOADING main PROGRAM
+namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Methodoverloading m1 = new Methodoverloading();
+            int resul1 = m1.Guide(30, 40);
+            Console.WriteLine(resul1);
+
+            // Methodoverloading m2 = new Methodoverloading();
+            double result2 = m1.Guide(50.05, 60.06);
+            Console.WriteLine(result2);
+
+        }
+    }
+}
